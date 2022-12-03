88a8ab85a2723886931dfafb73cbe41e592f7e2d
Sammy Humphreys <Sammyh2009@outlook.com>
2022-12-03 09:55:13 +0000

    Days 2 and 3

---
+++ AdventOfCode/Day02.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+
+namespace AdventOfCode;
+
+public class Day02 : BaseDay
+{
+    private readonly int[][] _input;
+    private readonly Hashtable result;
+    private readonly Hashtable win;
+    private readonly Hashtable lose;
+
+    public Day02()
+    {
+        _input = ParseInput();
+
+        result = new()
+        {
+            [0] = 2,    // Rock
+            [1] = 0,    // Paper
+            [2] = 1,    // Scissors
+        };
+
+        win = new()
+        {
+            [0] = 1,    // Paper
+            [1] = 2,    // Scissors
+            [2] = 0,    // Rock
+        };
+
+        lose = new()
+        {
+            [0] = 2,    // Rock
+            [1] = 0,    // Paper
+            [2] = 1,    // Scissors
+        };
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int total = 0;
+        foreach (var row in _input)
+        {
+            int them = row[0];
+            int us = row[1];
+
+            total += us + 1;
+
+            if ((int)result[them] == us) // Lose
+            {
+                total += 0;
+            }
+            else if (us == them) // Draw
+            {
+                total += 3;
+            }
+            else if ((int)result[us] == them) // Win
+            {
+                total += 6;
+            }
+        }
+
+        return new(total.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int total = 0;
+
+        foreach (var row in _input)
+        {
+            int them = row[0];
+            int us = -1;
+
+            int wanted = row[1];
+
+            if (wanted == 0)    // Lose
+            {
+                us = (int)lose[them];
+                total += 0;
+            }
+            else if (wanted == 1)   // Draw
+            {
+                us = them;
+                total += 3;
+            }
+            else if (wanted == 2)   // Draw
+            {
+                us = (int)win[them];
+                total += 6;
+            }
+
+            total += us + 1;
+        }
+
+        return new(total.ToString());
+    }
+
+    private int[][] ParseInput()
+    {
+        string[] input = File.ReadAllText(InputFilePath).Split("\n");
+        int[][] strategy = new int[input.Length][];
+
+        Hashtable convert = new()
+        {
+            ["A"] = 0,
+            ["B"] = 1,
+            ["C"] = 2,
+
+            ["X"] = 0,
+            ["Y"] = 1,
+            ["Z"] = 2,
+        };
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            string[] round = input[i].Split(" ");
+
+            strategy[i] = new int[] { (int)convert[round[0]], (int)convert[round[1]] };
+        }
+
+        return strategy;
+    }
+}
---
+++ AdventOfCode/Day03.cs
@@ -0,0 +1,63 @@
+﻿namespace AdventOfCode;
+
+public class Day03 : BaseDay
+{
+    private readonly string[] _input;
+
+    public Day03()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int result = 0;
+
+        string[][] split = new string[_input.Length][];
+
+        for (int i = 0; i < _input.Length; i++)
+        {
+            string line = _input[i];
+            split[i] = new string[2] { line.Substring(0, line.Length / 2), line.Substring(line.Length / 2, line.Length / 2) };
+        }
+
+        foreach (var line in split)
+        {
+           result += GetPriority(line[0].Intersect(line[1]).First());
+        }
+
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int result = 0;
+
+        for (int i = 0; i < _input.Length; i += 3)
+        {
+            result += GetPriority(_input[i].Intersect(_input[i + 1]).Intersect(_input[i + 2]).First());
+        }
+
+        return new(result.ToString());
+    }
+
+    private int GetPriority(char item)
+    {
+        if (item <= 'Z')
+        {
+            return item - 38;
+        }
+        else if (item <= 'z')
+        {
+            return item - 96;
+        }
+        return -1;
+    }
+
+    private string[] ParseInput()
+    {
+        string[] input = File.ReadAllText(InputFilePath).Split("\n");
+       
+        return input;
+    }
+}
