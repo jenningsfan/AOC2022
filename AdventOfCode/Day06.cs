b0c4e3608480d19aeb77b668a8911bae0a4c7aba
Sammy Humphreys <Sammyh2009@outlook.com>
2022-12-06 15:49:37 +0000

    Day 6

---
+++ AdventOfCode/Day06.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode;
+
+public class Day06 : BaseDay
+{
+    private readonly string _input;
+
+    public Day06()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1()
+    {
+        int result = 0;
+
+        for (int i = 0; i < _input.Length; i++)
+        {
+            if (new HashSet<char>(_input.Substring(i, 4).ToCharArray()).Count == 4)
+            {
+                result = i + 4;
+                break;
+            }
+        }
+
+        return new(result.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        int result = 0;
+
+        for (int i = 0; i < _input.Length; i++)
+        {
+            if (new HashSet<char>(_input.Substring(i, 14).ToCharArray()).Count == 14)
+            {
+                result = i + 14;
+                break;
+            }
+        }
+
+        return new(result.ToString());
+    }
+
+    private string ParseInput()
+    {
+        string input = File.ReadAllText(InputFilePath);
+
+        return input;
+    }
+}
